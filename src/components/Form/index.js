89423d6cc93674b63b00b1616052bbc96215e201
Laura Cole <lauracole1900@comcast.net>
2020-11-19 18:52:10 -0700

    started pseudocoding

---
+++ src/components/Form/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./style.css";
+
+// Dropdown button:
+// Select sort criteria
+// Last name
+// Role
+// Department
+// Date of hire
+
+// Submit button
+// onClick, sort by selected criteria
