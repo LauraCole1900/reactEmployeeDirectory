20e5af0246599c05b3d4fb328961ab1d617a38d9
Laura Cole <lauracole1900@comcast.net>
2020-11-20 22:09:07 -0700

    added styling

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
 import React, { Component } from "react";
 import TableData from "./components/Table";
 import employees from "./employees.json";
+import tardis from "./assets/tardis-clipart.png"
 import './App.css';
 
 class App extends Component {
 
   state = {
-    employeesArr: employees
+    employeesArr: employees,
+    sortAscending: true
   }
 
   render() {
     return (
-      <>
-        <h1>Employee Directory</h1>
+      <div className="container">
+        <div className="row">
+          <h1><img src={tardis} alt="TARDIS" className="titleImage" />
+          Employee Directory
+          <img src={tardis} alt="TARDIS" className="titleImage" /></h1>
+          {/* TARDIS image from https://webstockreview.net/explore/tardis-clipart-transparent-background/ */}
+        </div>
         <table>
-          <tr>
-            <td>
-              <span onClick={() => this.state.sort(this.state.id)} className="sort">ID</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.lastName)} className="sort">Last Name</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.firstName)} className="sort">First Name</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.role)} className="sort">Role</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.department)} className="sort">Department</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.phone)} className="sort">Phone</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.email)} className="sort">Email</span>
-            </td>
-            <td>
-              <span onClick={() => this.state.sort(this.state.hireDate)} className="sort">Date of Hire</span>
-            </td>
-          </tr>
+          <thead>
+            <tr>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.id)}>ID</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.lastName)}>Name</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.role)}>Role</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.department)}>Department</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.phone)}>Phone</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.email)}>Email</span>
+              </th>
+              <th className="tableHeader">
+                <span onClick={() => this.state.sort(this.state.hireDate)}>Date of Hire</span>
+              </th>
+            </tr>
+          </thead>
           {this.state.employeesArr.map(employee => (
             <TableData key={employee.id} {...employee}
               sortTable={this.sortTable} />
           ))}
         </table>
 
-      </>
+      </div>
     )
   }
 }
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,18 +3,17 @@ import "./style.css"
 
 function TableData(props) {
   return (
-    <>
+    <tbody>
       <tr>
         <td>{props.id}</td>
-        <td>{props.lastName}</td>
-        <td>{props.firstName}</td>
+        <td>{props.lastName}, {props.firstName}</td>
         <td>{props.role}</td>
         <td>{props.department}</td>
         <td>{props.phone}</td>
         <td>{props.email}</td>
         <td>{props.hireDate}</td>
       </tr>
-    </>
+    </tbody>
   )
 }
 
